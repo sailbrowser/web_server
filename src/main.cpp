8299c513e5c60c3960053713653e1511c197f687
Vitaly Voronin <vitl17@gmail.com>
2016-02-18 15:54:14 +0200

    demonize app

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 int main(int argc, char *argv[])
 {
@@ -35,6 +37,51 @@ int main(int argc, char *argv[])
   if(optind < 7) {
     std::cout << "Usage: " << argv[0] << " -h <ip> -p <port> -d <directory>" << std::endl;
     exit(EXIT_FAILURE);
+  }
+
+  pid_t pid, sid;
+
+  /* Fork off the parent process */
+  pid = fork();
+  if (pid < 0) {
+          exit(EXIT_FAILURE);
+  }
+  /* If we got a good PID, then
+     we can exit the parent process. */
+  if (pid > 0) {
+          exit(EXIT_SUCCESS);
+  }
+
+  /* Change the file mode mask */
+  umask(0);
+
+  /* Open any logs here */
+
+  /* Create a new SID for the child process */
+  sid = setsid();
+  if (sid < 0) {
+          /* Log any failures here */
+          exit(EXIT_FAILURE);
+  }
+
+
+  /* Change the current working directory */
+  if ((chdir("/")) < 0) {
+          /* Log any failures here */
+          exit(EXIT_FAILURE);
+  }
+
+  /* Close out the standard file descriptors */
+  close(STDIN_FILENO);
+  close(STDOUT_FILENO);
+  close(STDERR_FILENO);
+
+  /* Daemon-specific initialization goes here */
+
+  /* The Big Loop */
+  while (1) {
+     /* Do some task here ... */
+     sleep(30); /* wait 30 seconds */
   }
   return 0;
 }
